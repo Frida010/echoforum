80aeae9e35797f682b024ce381c84bebc280c1a4
Frida010 <>
2023-11-01 12:17:55 +0000

    Added django-allauth, to be able to login, logout and register accounts

--- forum/urls.py
+++ forum/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blogforum.urls'), name='blogforum_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
